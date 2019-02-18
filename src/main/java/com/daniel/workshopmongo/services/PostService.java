aafbe3f85ce066a7682d6773d3cf053121cb6495
Daniel Assuncao <daniel_dnl87@yahoo.com.br>
2019-02-18 10:32:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/daniel/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.daniel.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daniel.workshopmongo.domain.Post;
+import com.daniel.workshopmongo.repository.PostRepository;
+import com.daniel.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+}
